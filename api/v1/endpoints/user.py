08eebc96be86a158fe39020c75cf559f3f2c6786
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-05-20 10:01:16 +0900

    feat: Gemini 검색 Path추가 및 routers등록 (#6)

--- api/v1/endpoints/user.py
+++ api/v1/endpoints/user.py
@@ -4,39 +4,41 @@ from fastapi import APIRouter, Query, Form, HTTPException
 from sqlalchemy.orm import Session
 from starlette.responses import JSONResponse
 from core.dependencies import get_db
-from schemas.user import UserSignupRequest
+from schemas.user import UserSignupRequest, UserInputRequest
 from services.gemini_api import process_user_input
 from fastapi import Depends
 from services.user_service import user_signup_service
 
 router = APIRouter()
 
-# @router.post("/user/input")
-# def prompt_input(
-#     aiwi_id: Optional[int] = None,
-#     user_id: Optional[int] = None,
-#     query: str = Query(..., description="검색어"),
-#     db: Session = Depends(get_db),
-# ):
-#     try:
-#         result = process_user_input(query=query, aiwi_id=aiwi_id, user_id=user_id, db=db)
-#
-#         if "error" in result:
-#             return JSONResponse(status_code=500, content=result)
-#
-#         return JSONResponse(status_code=200, content={
-#             "status": "ok",
-#             "aiwi_id": result["aiwi_id"],
-#             "final_result": result["final_result"]
-#         })
-#     except Exception as e:
-#         return JSONResponse(status_code=500, content={"error": str(e)})
+@router.post("/user/input")
+def prompt_input(
+    request: UserInputRequest,
+    db: Session = Depends(get_db)
+):
+    try:
+        result = process_user_input(query=request.query, aiwi_id=request.aiwi_id, user_id=request.user_id, db=db)
+
+        if "error" in result:
+            return JSONResponse(status_code=500, content=result)
+
+        return JSONResponse(status_code=200, content={
+            "status": "ok",
+            "chat_id": result["chat_id"],
+            "final_result": result["final_result"]
+        })
+    except Exception as e:
+        response_data = {
+            "error발생!!": str(e),
+            "result": result  # result 전체를 error 정보와 함께 보냄
+        }
+        return JSONResponse(status_code=500, content=response_data)
+
 @router.post("/user/signup")
 def user_signup(
         request: UserSignupRequest,
         db: Session = Depends(get_db)
 ):
-
     try:
         user_signup_service(
             email= request.email,
