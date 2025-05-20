94e11a83ba2b20169577c6dffecd79759ef3729d
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-05-20 09:45:29 +0900

    fix: 이름 오류 및 DB자료형 변환 (#1) - aiwi: aiwi_id -> chat_id - aiwi_log: aiwi_id -> chat_id 	- ai_input1 -> ai_output1 	- ai_input2 -> ai_output2 	- ai_input3 -> ai_output3 자료형 오류 - String(255)로 Ai의 결과값을 저장하려고 했지만 너무 긴 출력이 나왔을 때 담지 못하는 오류가 발생했다. - 그래서 String(255) -> Text로 변경했습니다.
    refs: #1

--- models/aiwi.py
+++ models/aiwi.py
@@ -6,7 +6,7 @@ class Aiwi(base):
 
     __tablename__ = "aiwi"
 
-    aiwi_id = Column(Integer, primary_key=True, index=True)
+    chat_id = Column(Integer, primary_key=True, index=True)
     title = Column(String(255), nullable=False)
     user_id = Column(Integer, ForeignKey("users.user_id"),nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
