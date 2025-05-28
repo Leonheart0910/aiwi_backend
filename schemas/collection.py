03114a52b373a8ac15371840c76bb540699a74eb
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-05-28 14:18:39 +0900

    feat: item, collection, image의 response schema 생성(#11)

--- schemas/collection.py
+++ schemas/collection.py
@@ -3,7 +3,8 @@ from typing import Optional
 from pydantic import BaseModel
 
 from typing import List
-from schemas.item import ItemOut
+from schemas.item import ItemOut, ItemInfo
+
 
 class CollectionCreate(BaseModel):
     collection_title: str
@@ -33,3 +34,13 @@ class CollectionWithItems(BaseModel):
 class CollectionCreateRequest(BaseModel):
     user_id : int
     collection_title: str
+
+class CollectionItemList(BaseModel):
+    collection_id: int
+    collection_title: str
+    user_id: int
+    created_at: Optional[datetime]
+    items: List[ItemInfo] = []
+
+    class Config:
+        from_attributes = True
