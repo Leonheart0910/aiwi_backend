98479f423e16b49245d0c23bff59c1b0f30c7ec6
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-05-28 14:54:23 +0900

    feat: 장바구니 아이템 삭제 기능 추가(#12)

--- api/v1/endpoints/collection.py
+++ api/v1/endpoints/collection.py
@@ -5,7 +5,8 @@ from sqlalchemy.orm import Session
 
 from core.dependencies import get_db
 from schemas.collection import CollectionCreateRequest
-from services.collection_service import collection_create_service, get_collection_items_service
+from services.collection_service import collection_create_service, get_collection_items_service, \
+    delete_collection_item_service
 
 router = APIRouter()
 
@@ -43,3 +44,16 @@ def get_collection_items(
         collection_id=collection_id,
         db=db
     )
+
+@router.delete(
+    "/collection/{collection_id}/{item_id}"
+)
+def delete_collection_item(
+        collection_id: int,
+        item_id: int,
+        db : Session = Depends(get_db)
+):
+    try:
+        return delete_collection_item_service(collection_id=collection_id, item_id=item_id, db=db)
+    except HTTPException as e:
+        raise e
