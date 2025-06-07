62dfb26bd59f51464de895bf7ad5425167a60b98
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-06-07 22:34:36 +0900

    fix: 장바구니의 아이템 삭제 부분 에러 수정 (#29)

--- crud/collection.py
+++ crud/collection.py
@@ -45,22 +45,24 @@ def delete_collection_items_by_id(db: Session,
     if not collection:
         raise HTTPException(status_code=404, detail="장바구니를 찾을 수 없습니다")
 
-    item = db.query(Item).filter(
+    item = db.query(Item) \
+        .options(
+        joinedload(Item.product).
+        joinedload(Product.product_info)
+    ) \
+        .filter(
         Item.item_id == item_id,
-        Item.collection_id == collection_id  # ← 이게 핵심
-    ).first()
+        Item.collection_id == collection_id
+    ) \
+        .first()
 
     if not item:
         raise HTTPException(status_code=404, detail="아이템이 장바구니 안에 없습니다")
 
-    try:
-        product_name = item.product.product_name
-    except AttributeError:
-        # 만약 item.product_name 속성을 직접 가지고 있다면 바로 꺼내도 됨
-        product_name = getattr(item, "product_name", "")
-
+    product_name = ""
+    if item.product and item.product.product_info:
+        product_name = item.product.product_info[0].product_name
 
     db.delete(item)
     db.commit()
-
     return {"message": f"{collection.collection_title}에 들어있는 {product_name} 가 삭제되었습니다."}
